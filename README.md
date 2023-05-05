e6c36a2d8ab4fdd3e975852bfd7dbcfffe5c6640
doublebyte <doublebyte@hushmail.com>
2023-05-05 14:21:38 +0100

    - initial import

---
+++ README.md
@@ -0,0 +1,27 @@
+# ETL Tools
+
+<img src="https://raw.githubusercontent.com/doublebyte1/yellow-bricks/master/dist/assets/img/portfolio/ecities.svg" width="200">
+
+Helper tools to insert data into the sdi
+
+## Quick Setup
+
+Convert Shapefiles on different folders to the eMOTIONAL Cities recommended formats: `GeoJSON` and `GeoPackage`.
+
+```
+chmod +x convert-shp.sh
+```
+```
+./convert-shp.sh
+```
+
+## Requirements
+
+* [GDAL](https://gdal.org/) >= 3.7
+* [jq](https://stedolan.github.io/jq/)
+
+## License
+
+This project is released under an [MIT License](./LICENSE)
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
---
+++ convert-shp.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#Note: gdal > 3.7
+
+walk_dir () {    
+    shopt -s nullglob dotglob
+
+    for pathname in "$1"/*; do
+        if [ -d "$pathname" ]; then
+            walk_dir "$pathname"
+        else
+            case "$pathname" in
+                *.shp)
+                    #printf '%s\n' "$pathname"
+                    #ogrinfo "$pathname"
+                    LAYER=$($GDAL_PATH/ogrinfo -json "$pathname" | jq '.layers[0].name')
+                    #CRS=$("$GDAL_PATH"/ogrinfo -json "$pathname" | jq '.layers[0].geometryFields[0].coordinateSystem.projjson.id.code')
+                    echo "Converting $LAYER to GeoJSON"
+                    OUT=$(echo $LAYER.geojson | tr -d '"')
+                    #echo $OUT
+                    $GDAL_PATH/ogr2ogr $OUTPUT_DIR/$OUT.geojson "$pathname" -f GeoJSON -t_srs EPSG:4326
+                    echo "Converting $LAYER to GeoPackage"
+                    $GDAL_PATH/ogr2ogr $OUTPUT_DIR/$OUT.gpkg "$pathname" -f GPKG -t_srs EPSG:4326
+                    exit
+            esac
+        fi
+    done
+}
+
+GDAL_PATH=/home/joana/apps/bin/
+DOWNLOADING_DIR=/home/joana/projects/EC/msu
+OUTPUT_DIR=/home/joana/projects/EC/msu/output
+
+walk_dir "$DOWNLOADING_DIR"
