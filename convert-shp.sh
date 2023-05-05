781157ee96c5947f7843f97a8eb30382fbc6913f
doublebyte <doublebyte@hushmail.com>
2023-05-05 14:00:17 +0100

    - fixed output filename

--- convert-shp.sh
+++ convert-shp.sh
@@ -15,11 +15,12 @@ walk_dir () {
                     LAYER=$($GDAL_PATH/ogrinfo -json "$pathname" | jq '.layers[0].name')
                     #CRS=$("$GDAL_PATH"/ogrinfo -json "$pathname" | jq '.layers[0].geometryFields[0].coordinateSystem.projjson.id.code')
                     echo "Converting $LAYER to GeoJSON"
-                    OUT=$(echo $LAYER.geojson | tr -d '"')
+                    OUT=$(echo $LAYER | tr -d '"')
                     #echo $OUT
                     $GDAL_PATH/ogr2ogr $OUTPUT_DIR/$OUT.geojson "$pathname" -f GeoJSON -t_srs EPSG:4326
                     echo "Converting $LAYER to GeoPackage"
                     $GDAL_PATH/ogr2ogr $OUTPUT_DIR/$OUT.gpkg "$pathname" -f GPKG -t_srs EPSG:4326
+                    exit
             esac
         fi
     done
