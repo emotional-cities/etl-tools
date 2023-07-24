0413249a013a326001845c303915278d1b255545
doublebyte <doublebyte@hushmail.com>
2023-07-24 19:43:23 +0100

    - export library_path

--- convert-gpkg.sh
+++ convert-gpkg.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #Note: gdal > 3.7
 
+export LD_LIBRARY_PATH=$LD_PATH
+
 walk_dir () {    
     shopt -s nullglob dotglob
 
@@ -16,14 +18,15 @@ walk_dir () {
                     #echo "Converting $LAYER to GeoPackage"
                     OUT=$(echo $LAYER | tr -d '"')
                     OUT_L=$(echo "$OUT" | tr '[:upper:]' '[:lower:]')
-                    $GDAL_PATH/ogr2ogr $OUTPUT_DIR/$OUT_L.gpkg "$pathname" -f GPKG -oo X_POSSIBLE_NAMES="GPS (Long.) [deg]" -oo Y_POSSIBLE_NAMES="GPS (Lat.) [deg]" -oo Y_POSSIBLE_NAMES="GPS (Alt.) [m]" -a_srs 'EPSG:4326'
+                    export LD_LIBRARY_PATH=$LD_PATH && $GDAL_PATH/ogr2ogr $OUTPUT_DIR/$OUT_L.gpkg "$pathname" -f GPKG -oo X_POSSIBLE_NAMES="GPS (Long.) [deg]" -oo Y_POSSIBLE_NAMES="GPS (Lat.) [deg]" -oo Y_POSSIBLE_NAMES="GPS (Alt.) [m]" -a_srs 'EPSG:4326'
              esac
         fi
     done
 }
 
-GDAL_PATH=/home/joana/apps/bin/
-DOWNLOADING_DIR=/home/joana/projects/EC/fmul/Video\ data/Videos/
+LD_PATH=/home/joana/apps/lib
+GDAL_PATH=/home/joana/apps/bin
+DOWNLOADING_DIR=/home/joana/projects/EC/fmul/Video\ data/Videos
 OUTPUT_DIR=/home/joana/projects/EC/fmul/output/
 
 walk_dir "$DOWNLOADING_DIR"
